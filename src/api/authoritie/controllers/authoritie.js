ac36546b8c673bfaab078063cb8057821a13512f
nogvs <vicsnogueira@hotmail.com>
2023-11-24 00:52:30 -0300

    Primeiro commit

---
+++ src/api/authoritie/controllers/authoritie.js
@@ -0,0 +1,9 @@
+'use strict';
+
+/**
+ * authoritie controller
+ */
+
+const { createCoreController } = require('@strapi/strapi').factories;
+
+module.exports = createCoreController('api::authoritie.authoritie');
---
+++ src/api/authoritie/routes/authoritie.js
@@ -0,0 +1,9 @@
+'use strict';
+
+/**
+ * authoritie router
+ */
+
+const { createCoreRouter } = require('@strapi/strapi').factories;
+
+module.exports = createCoreRouter('api::authoritie.authoritie');
